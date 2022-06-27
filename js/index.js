e4336142403de3cd40ba62fbcc4a7b7f32de52c0
Jihen RAYSSI <jihenraissi88@gmail.com>
2022-06-27 14:30:53 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -1,29 +1,34 @@
 $(document).ready(function() {
+    $(".first-list-element").addClass("active");
+    //Delete the active class and place it when one of the top menu buttons is pressed
     $(".nav-item").click(function () {
-        $("nav-item").removeClass("active");
+        $(".nav-item").removeClass("active");
         $(this).addClass("active");
     });
-
+    // when you click on the icon of the side menu
     $("#toggler").click(function(event) {
         $('#wrap').toggleClass('toggled');
-        
-        var left = $('.sidebar').css("left");
-        if (left == '0px')
+
+       // add a black layer
+        var right = $('.sidebar').css("left"); // we have given this property to the side menu so that it is on the right
+        if (right == '0px') // the side menu is showing and should be closed
         {
             $('.sidebar').css({ 'left': '-17rem' });
-            $('.layer').fadeOut();
+            $('.layer').fadeOut(); /*hide the black layer*/
         }
-        else{
+        else { // the side menu is closed and should be shown
             $('.sidebar').css({ 'left': '0' });
-            $('.layer').fadeIn();
+            $('.layer').fadeIn(); /*show black layer*/
         }
     });
 
+    // Hide the side menu and the black layer when clicking on the black layer
     $('.layer').click(function () {
         $('.sidebar').css({ 'left': '-17rem' });
         $('.layer').fadeOut();
     });
 
+   // Hide and show the search field when pressing the search icon that appears in the medium and small screens
     $(".search-icon").click(function () {
         $(".search-input").slideToggle("slow"); 
     });
